f3183ca176dbbbd152a89c5f395202be289f19d4
Mr.Lin <linsongbin@126.com>
2020-09-23 16:37:25 +0800

    remove gateway

--- src/MqttFx/Client/MqttClient.cs
+++ src/MqttFx/Client/MqttClient.cs
@@ -8,6 +8,10 @@ using Microsoft.Extensions.Options;
 using MqttFx.Channels;
 using MqttFx.Utils;
 using System;
+using System.Buffers;
+using System.IO.Pipelines;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -16,13 +20,15 @@ namespace MqttFx.Client
     /// <summary>
     /// Mqtt客户端
     /// </summary>
-    public class MqttClient : IMqttClient
+    public class MqttClient
     {
         private readonly ILogger _logger;
         private IEventLoopGroup _eventLoop;
         private volatile IChannel _channel;
         private readonly PacketIdProvider _packetIdProvider = new PacketIdProvider();
         private readonly PacketDispatcher _packetDispatcher = new PacketDispatcher();
+
+        private Socket clientSocket;
 
         public bool IsConnected { get; private set; }
 
@@ -44,36 +50,32 @@ namespace MqttFx.Client
         /// 连接
         /// </summary>
         /// <returns></returns>
-        public async ValueTask<MqttConnectResult> ConnectAsync(CancellationToken cancellationToken)
+        public async ValueTask<bool> ConnectAsync(EndPoint remoteEndPoint, CancellationToken cancellationToken)
         {
-            if (_eventLoop == null)
-                _eventLoop = new MultithreadEventLoopGroup();
-
-            var connectFuture = new TaskCompletionSource<MqttConnectResult>();
-            var bootstrap = new Bootstrap();
-            bootstrap
-                .Group(_eventLoop)
-                .Channel<TcpSocketChannel>()
-                .Option(ChannelOption.TcpNodelay, true)
-                .RemoteAddress(Options.Host, Options.Port)
-                .Handler(new MqttChannelInitializer(this, connectFuture));
-
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             try
             {
-                _channel = await bootstrap.ConnectAsync();
-                if (_channel.Open)
+                await clientSocket.ConnectAsync(remoteEndPoint);
+
+                await Task.Factory.StartNew(async (state) =>
                 {
-                    _packetDispatcher.Reset();
-                    _packetIdProvider.Reset();
-                    IsConnected = true;
-                }
-                return await connectFuture.Task;
+                    var session = (Socket)state;
+                    if (_logger.IsEnabled(LogLevel.Information))
+                    {
+                        _logger.LogInformation($"[Connected]:{session.LocalEndPoint} to {session.RemoteEndPoint}");
+                    }
+                    var pipe = new Pipe();
+                    Task writing = FillPipeAsync(session, pipe.Writer, cancellationToken);
+                    Task reading = ReadPipeAsync(session, pipe.Reader);
+                    await Task.WhenAll(reading, writing);
+                }, clientSocket);
             }
             catch (Exception ex)
             {
                 _logger.LogError(ex, ex.Message);
                 throw new MqttException("BrokerUnavailable: " + ex.Message);
             }
+            return true;
         }
 
         /// <summary>
@@ -149,6 +151,122 @@ namespace MqttFx.Client
                 return _channel.WriteAndFlushAsync(packet);
 
             return Task.CompletedTask;
+        }
+
+        private async Task FillPipeAsync(Socket session, PipeWriter writer, CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                try
+                {
+                    Memory<byte> memory = writer.GetMemory(8096);
+                    int bytesRead = await session.ReceiveAsync(memory, SocketFlags.None, cancellationToken);
+                    if (bytesRead == 0)
+                    {
+                        break;
+                    }
+                    writer.Advance(bytesRead);
+                }
+                catch (SocketException ex)
+                {
+                    _logger.LogError($"[{ex.SocketErrorCode},{ex.Message}]:{session.RemoteEndPoint}");
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, $"[Receive Error]:{session.RemoteEndPoint}");
+                    break;
+                }
+                FlushResult result = await writer.FlushAsync();
+                if (result.IsCompleted)
+                {
+                    break;
+                }
+            }
+            writer.Complete();
+        }
+
+        private async Task ReadPipeAsync(Socket session, PipeReader reader)
+        {
+            while (true)
+            {
+                ReadResult result = await reader.ReadAsync();
+                if (result.IsCompleted)
+                {
+                    break;
+                }
+                ReadOnlySequence<byte> buffer = result.Buffer;
+                SequencePosition consumed = buffer.Start;
+                SequencePosition examined = buffer.End;
+                try
+                {
+                    if (result.IsCanceled) break;
+                    if (buffer.Length > 0)
+                    {
+                        ReaderBuffer(ref buffer, session, out consumed, out examined);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //Close();
+                    break;
+                }
+                finally
+                {
+                    reader.AdvanceTo(consumed, examined);
+                }
+            }
+            reader.Complete();
+        }
+
+        private void ReaderBuffer(ref ReadOnlySequence<byte> buffer, Socket session, out SequencePosition consumed, out SequencePosition examined)
+        {
+            consumed = buffer.Start;
+            examined = buffer.End;
+            SequenceReader<byte> seqReader = new SequenceReader<byte>(buffer);
+            if (seqReader.TryPeek(out byte beginMark))
+            {
+                //if (beginMark != JT808Package.BeginFlag) throw new ArgumentException("Not JT808 Packages.");
+            }
+            byte mark = 0;
+            long totalConsumed = 0;
+            while (!seqReader.End)
+            {
+                //if (seqReader.IsNext(JT808Package.BeginFlag, advancePast: true))
+                //{
+                //    if (mark == 1)
+                //    {
+                //        try
+                //        {
+                //            var package = JT808Serializer.HeaderDeserialize(seqReader.Sequence.Slice(totalConsumed, seqReader.Consumed - totalConsumed).ToArray(), minBufferSize: 8096);
+                //            ReceiveAtomicCounterService.MsgSuccessIncrement();
+                //            if (Logger.IsEnabled(LogLevel.Debug)) Logger.LogDebug($"[Atomic Success Counter]:{ReceiveAtomicCounterService.MsgSuccessCount}");
+                //            if (Logger.IsEnabled(LogLevel.Trace)) Logger.LogTrace($"[Accept Hex {session.RemoteEndPoint}]:{package.OriginalData.ToArray().ToHexString()}");
+                //        }
+                //        catch (JT808Exception ex)
+                //        {
+                //            Logger.LogError(ex, $"[HeaderDeserialize ErrorCode]:{ ex.ErrorCode}");
+                //        }
+                //        totalConsumed += (seqReader.Consumed - totalConsumed);
+                //        if (seqReader.End) break;
+                //        seqReader.Advance(1);
+                //        mark = 0;
+                //    }
+                //    mark++;
+                //}
+                //else
+                //{
+                //    seqReader.Advance(1);
+                //}
+            }
+            if (seqReader.Length == totalConsumed)
+            {
+                examined = consumed = buffer.End;
+            }
+            else
+            {
+                consumed = buffer.GetPosition(totalConsumed);
+            }
         }
     }
 }
